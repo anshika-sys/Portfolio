9ec3f41f7522c7709f26689bbbb7dd8304a653b5
anshika-sys <anshikatyagi924@gmail.com>
2025-09-01 10:38:18 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Enthusiastic Student", "AI Student", "Music Lover", "Hardworking Person"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
